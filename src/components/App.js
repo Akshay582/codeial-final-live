ec5dc090ccfbcbfdb216af59bb74849e1e8d35ec
Akshay Mittal <akshaymittal.da@gmail.com>
2020-05-10 19:17:55 +0530

    Add UI for posts list only

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
 
 class App extends Component {
   componentDidMount() {
@@ -12,24 +13,7 @@ class App extends Component {
     const { posts } = this.props;
     return (
       <div>
-        <div className="posts-list">
-          {posts.map((post) => {
-            <div className="post-wrapper" key={post._id}>
-              <div className="post-header">
-                <div className="post-avatar">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-pic"
-                  />
-                  <div>
-                    <span className="post-author">{post.user.name}</span>
-                    <span className="post-time">a minute ago</span>
-                  </div>
-                </div>
-              </div>
-            </div>;
-          })}
-        </div>
+        <PostsList posts={posts} />
       </div>
     );
   }
